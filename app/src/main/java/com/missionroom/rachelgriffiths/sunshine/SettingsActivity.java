31661819c2df3c77b59248b76f54795129fb001c
Rachel Griffiths <thisisrachelalice@hotmail.com>
2015-04-15 11:58:54 +0100

    Revert "added binding pref key"
    This reverts commit 212b0eeadf26956d934775d2d1ffcd258be2b770.

--- app/src/main/java/com/missionroom/rachelgriffiths/sunshine/SettingsActivity.java
+++ app/src/main/java/com/missionroom/rachelgriffiths/sunshine/SettingsActivity.java
@@ -38,7 +38,6 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
         addPreferencesFromResource(R.xml.pref_general);
         //code to load the below method
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
     }
 
     //this is to attach a listener to preferences so that the summary gets updated
